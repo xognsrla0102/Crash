e4afd74203861d589bdf258d7a2c4c680e579992
김태훈 <xognsrla0102@gmail.com>
2022-05-20 11:38:11 +0900

    프로젝트 세팅

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Test : MonoBehaviourPunCallbacks
+{
+    public InputField nickNameInputField;
+
+    public void OnClickConnect()
+    {
+        print($"닉네임 : {nickNameInputField.text}으로 연결");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.NickName = nickNameInputField.text;
+        print("연결 완료");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print($"연결 끊김 : {cause}");
+    }
+}
