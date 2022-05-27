61ebc5eea030f0e95386298a13a2cc0705f965d6
김태훈 <xognsrla0102@gmail.com>
2022-05-27 15:16:05 +0900

    포톤, 플레이팹, 닷트윈, 기타 프로젝트 추가 세팅

---
+++ Assets/Scripts/MovementInputField.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MovementInputField : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            EventSystem.current.currentSelectedGameObject
+                ?.GetComponent<Selectable>()
+                .FindSelectableOnDown()
+                ?.Select();
+        }
+    }
+}
