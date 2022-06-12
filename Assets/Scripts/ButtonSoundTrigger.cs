9891fbcd62a1f3043b7dd922cd7c157a80b5b0c5
김태훈 <xognsrla0102@gmail.com>
2022-06-12 20:16:23 +0900

    버튼 사운드 적용

---
+++ Assets/Scripts/ButtonSoundTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonSoundTrigger : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        SoundManager.Instance.PlaySND(ESfxName.BUTTON_OVER_SFX);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SoundManager.Instance.PlaySND(ESfxName.BUTTON_CLICK_SFX);
+    }
+}
