f925cc4a4ff2634664839a15b1439b2f252da562
김태훈 <xognsrla0102@gmail.com>
2022-06-16 19:19:30 +0900

    로비씬 추가

---
+++ Assets/Scripts/Scenes/LobbyScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyScene : MonoBehaviour
+{
+    [SerializeField] private Button titleBtn;
+
+    private void Start()
+    {
+        titleBtn.onClick.AddListener(OnClickTitleBtn);
+    }
+
+    private void OnDestroy()
+    {
+        titleBtn.onClick.RemoveAllListeners();
+    }
+
+    private void OnClickTitleBtn()
+    {
+        LoadingManager.LoadScene(ESceneName.TITLE_SCENE);
+    }
+}
