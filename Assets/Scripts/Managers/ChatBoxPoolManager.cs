d44fbec9192f3a484f6dc91a66efef5951bb9d10
김태훈 <xognsrla0102@gmail.com>
2022-06-26 21:40:56 +0900

    채팅 박스 오브젝트 풀링 로직 수정

--- Assets/Scripts/Managers/ChatBoxPoolManager.cs
+++ Assets/Scripts/Managers/ChatBoxPoolManager.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 
 public class ChatBoxPoolManager : Singleton<ChatBoxPoolManager>
 {
-    [SerializeField] private int addPoolObjCnt;
+    [HideInInspector] public const int SHOW_CHAT_BOX_CNT = 20;
+
+    // 만약 풀링 오브젝트를 다 쓸 경우, 추가로 풀링 오브젝트를 할당할 숫자, 나중에 필요해지면 쓸 것
+    // [HideInInspector] public const int ADD_POOL_OBJ_CNT = 10;
+
     [SerializeField] private GameObject chatBoxObj;
 
     private Queue<GameObject> poolObjQueue = new Queue<GameObject>();
 
     private void Awake()
     {
-        AddPoolObj(addPoolObjCnt);
+        AddPoolObj(SHOW_CHAT_BOX_CNT);
     }
 
     private void AddPoolObj(int addCnt = 1)
@@ -37,10 +41,10 @@ public class ChatBoxPoolManager : Singleton<ChatBoxPoolManager>
 
     public GameObject Pop(Transform parent = null)
     {
-        // 풀링된 오브젝트를 다 쓴 경우 풀링 오브젝트 더 생성
+        // 풀링된 오브젝트를 다 쓴 경우에 Pop을 하는 경우는 버그
         if (poolObjQueue.Count == 0)
         {
-            AddPoolObj(addPoolObjCnt);
+            Debug.Assert(false, "풀링 오브젝트를 전부 사용하였습니다.");
         }
 
         GameObject retObj = poolObjQueue.Dequeue();
