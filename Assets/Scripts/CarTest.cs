21f01bd918bd8c2ad98363dea151734c1134de28
김태훈 <xognsrla0102@gmail.com>
2022-07-09 13:26:01 +0900

    중간 저장

---
+++ Assets/Scripts/CarTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+
+public class CarTest : MonoBehaviourPunCallbacks
+{
+    public GameObject[] spawnPos;
+
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomOrCreateRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("Prefabs/Player", spawnPos[Random.Range(0, spawnPos.Length)].transform.position, Quaternion.identity);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,28 +1,94 @@
 ﻿using UnityEngine;
+using System.Collections;
+using Photon.Pun;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
 {
     [SerializeField] private float spd;
-    [SerializeField] private float gravity;
 
-    public ForceMode forceMode;
-    private CharacterController characterController;
     private Rigidbody rigid;
+    private Vector3 nowPos;
+    private bool isCrash;
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            nowPos = (Vector3)stream.ReceiveNext();
+            transform.rotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
 
     private void Awake()
     {
-        characterController = GetComponent<CharacterController>();
         rigid = GetComponent<Rigidbody>();
-        rigid.AddForce(Vector3.up * 5, forceMode);
+
+        transform.Find(photonView.IsMine ? "CarBlue" : "CarRed").gameObject.SetActive(true);
+    }
+
+    private void FixedUpdate()
+    {
+        if (photonView.IsMine)
+        {
+            if (isCrash)
+            {
+                return;
+            }
+
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            Vector3 moveDir = new Vector3(h, 0, v).normalized;
+            transform.LookAt(transform.position + moveDir);
+            rigid.AddForce(moveDir * spd, ForceMode.Acceleration);
+        }
     }
 
     private void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-        Vector3 moveDir = new Vector3(h, 0, v).normalized * Time.unscaledDeltaTime;
-        transform.LookAt(transform.position + moveDir);
-        // moveDir.y -= gravity * Time.unscaledDeltaTime;
-        rigid.AddForce(moveDir);
+        if (photonView.IsMine)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                CarTest carTest = FindObjectOfType<CarTest>();
+                transform.position = carTest.spawnPos[Random.Range(0, carTest.spawnPos.Length)].transform.position;
+                rigid.velocity = Vector3.zero;
+            }
+        }
+        else if ((transform.position - nowPos).sqrMagnitude >= 50)
+        {
+            transform.position = nowPos;
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, nowPos, Time.deltaTime * 10);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(CrashCoroutine(collision.gameObject.GetComponent<Rigidbody>().velocity));
+        }
+    }
+
+    private IEnumerator CrashCoroutine(Vector3 enemyVelocity)
+    {
+        print("충돌");
+        isCrash = true;
+        Vector3 oldVelocity = rigid.velocity;
+        rigid.velocity = Vector3.zero;
+
+        yield return new WaitForSeconds(0.5f);
+
+        print("충돌해제 및 충격파");
+        isCrash = false;
+        Vector3 totalVelocity = enemyVelocity + oldVelocity;
+        rigid.AddForce(totalVelocity * 3f, ForceMode.Impulse);
     }
 }
